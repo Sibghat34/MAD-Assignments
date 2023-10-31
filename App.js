011fef751bf8a4ddcd21511e9ff7d0f60e296f6f
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-10-31 22:49:21 +0500

    assignment completed

--- App.js
+++ App.js
@@ -1,15 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { ListItem, Avatar, Text } from "react-native-elements";
+
+const data = Array.from({ length: 25 }, (_, i) => ({
+  id: i.toString(),
+  name: `User ${i + 1}`,
+  message: `This is message #${i + 1}`,
+  time: `${Math.floor(Math.random() * 12)}:${Math.floor(Math.random() * 60)} ${
+    Math.random() > 0.5 ? "AM" : "PM"
+  }`,
+  avatar: `https://randomuser.me/api/portraits/thumb/men/${i}.jpg`,
+}));
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount((prevCount) => prevCount + 1);
+
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      {/* <View style={styles.headingWrapper}>
+        <TouchableOpacity onPress={onPress} style={styles.topButton}>
+          <Text style={styles.heading}>WhatCount</Text>
+          <Text style={styles.heading1}>{count}</Text>
+        </TouchableOpacity>
+      </View> */}
+      <View style= {styles.main}>
+      <FlatList 
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ListItem bottomDivider >
+            <Avatar rounded source={{ uri: item.avatar }}  />
+            <ListItem.Content >
+              <ListItem.Title>{item.name}</ListItem.Title>
+              <ListItem.Subtitle>{item.message}</ListItem.Subtitle>
+            </ListItem.Content>
+            <View style={{ alignItems: "flex-end" }}>
+              <Text style={{ color: "gray" }}>{item.time}</Text>
+            </View>
+          </ListItem>
+        )}
+      />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
- 
+main:{
+  marginTop: 30,
+}
 });
